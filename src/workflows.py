95963e67dbcba683acd460a2f3c577f3fad033e0
cberganz <charles.berganza@gmail.com>
2023-06-23 16:59:08 +0200

    fix: edit authorization to Bearer $apiKey

--- src/workflows.py
+++ src/workflows.py
@@ -4,7 +4,7 @@ from .types import WorkflowRun
 
 class Workflows:
     def __init__(self, apiKey: str) -> None:
-        self.headers: Dict[str, str] = {'authorization': apiKey}
+        self.headers: Dict[str, str] = {'authorization': f'Bearer {apiKey}'}
         with open('../config.json') as config_file:
             config = json.load(config_file)
         self.apiUrl: str = config['apiUrl']
