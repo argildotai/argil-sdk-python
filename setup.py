4d4d8e453c4742093f8a47630766c83274a6e77e
cberganz <charles.berganza@gmail.com>
2023-07-05 19:23:54 +0200

    Package done - v0.0.1

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
 
 setup(
-    name='argil-sdk',
-    version='0.1.0',
+    name='argil',
+    version='{{VERSION_PLACEHOLDER}}',
+    author="Brivael Le Pogam",
+    author_email="briva@argil.ai",
     description='SDK for the Argil API',
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        # "Programming Language :: Python :: 3 :: Only",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ],
     install_requires=[
         'requests',
+        'typing',
     ],
 )
